02b4b893be53c3f824ba2acfa2d00f277ed0e5bd
Allen Chen <Allen@INSELLERATE.LOCAL>
2016-08-24 19:02:24 -0700

    finish classes

--- app/classes/scripts.js
+++ app/classes/scripts.js
@@ -1,3 +1,8 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var main;
 (function (main) {
     var classes = (function () {
@@ -21,12 +26,159 @@ var main;
             });
             return this;
         };
+        classes.prototype.a2 = function () {
+            document.getElementById("a2")
+                .addEventListener("click", function () {
+                this.innerText =
+                    "Error: 'name' is private";
+            });
+            return this;
+        };
+        classes.prototype.a3 = function () {
+            document.getElementById("a3")
+                .addEventListener("click", function () {
+                this.innerText =
+                    "yes";
+            });
+            return this;
+        };
+        classes.prototype.a4 = function () {
+            document.getElementById("a4")
+                .addEventListener("click", function () {
+                this.innerText =
+                    "no";
+            });
+            return this;
+        };
+        classes.prototype.a5 = function () {
+            var Person = (function () {
+                function Person(name) {
+                    this.name = name;
+                }
+                return Person;
+            }());
+            var Employee = (function (_super) {
+                __extends(Employee, _super);
+                function Employee(name, department) {
+                    _super.call(this, name);
+                    this.department = department;
+                }
+                Employee.prototype.getElevatorPitch = function () {
+                    return "Hello, my name is " + this.name + " and I work in " + this.department + ".";
+                };
+                return Employee;
+            }(Person));
+            document.getElementById("a5")
+                .addEventListener("click", function () {
+                var howard = new Employee("Howard", "Sales");
+                this.innerText =
+                    howard.getElevatorPitch();
+            });
+            return this;
+        };
+        classes.prototype.a6 = function () {
+            document.getElementById("a6")
+                .addEventListener("click", function () {
+                this.innerText =
+                    "error";
+            });
+            return this;
+        };
+        classes.prototype.a7 = function () {
+            var passcode = "secret passcode";
+            var Employee = (function () {
+                function Employee() {
+                }
+                Object.defineProperty(Employee.prototype, "fullName", {
+                    get: function () {
+                        return this._fullName;
+                    },
+                    set: function (newName) {
+                        if (passcode && passcode == "secret passcode") {
+                            this._fullName = newName;
+                        }
+                        else {
+                            console.log("Error: Unauthorized update of employee!");
+                        }
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                return Employee;
+            }());
+            document.getElementById("a7")
+                .addEventListener("click", function () {
+                var employee = new Employee();
+                employee.fullName = "Bob Smith";
+                if (employee.fullName) {
+                    console.log(employee.fullName);
+                }
+                this.innerText =
+                    "" + employee.fullName;
+            });
+            return this;
+        };
+        classes.prototype.a89 = function () {
+            var Grid = (function () {
+                function Grid(scale) {
+                    this.scale = scale;
+                }
+                Grid.prototype.calculateDistanceFromOrigin = function (point) {
+                    var xDist = (point.x - Grid.origin.x);
+                    var yDist = (point.y - Grid.origin.y);
+                    return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+                };
+                Grid.origin = { x: 0, y: 0 };
+                return Grid;
+            }());
+            var grid1 = new Grid(1.0); // 1x scale
+            var grid2 = new Grid(5.0); // 5x scale
+            document.getElementById("a8")
+                .addEventListener("click", function () {
+                this.innerText =
+                    "" + grid1.calculateDistanceFromOrigin({ x: 10, y: 10 });
+            });
+            document.getElementById("a9")
+                .addEventListener("click", function () {
+                this.innerText =
+                    "" + grid2.calculateDistanceFromOrigin({ x: 10, y: 10 });
+            });
+            return this;
+        };
+        classes.prototype.a10 = function () {
+            var Dog = (function (_super) {
+                __extends(Dog, _super);
+                function Dog() {
+                    _super.call(this, "puppy");
+                }
+                Dog.prototype.talk = function () {
+                    return this.name + " is talking";
+                };
+                return Dog;
+            }(Animal));
+            document.getElementById("a10")
+                .addEventListener("click", function () {
+                var dog = new Dog();
+                this.innerText =
+                    "" + dog.talk();
+            });
+            return this;
+        };
         classes.prototype.start = function () {
-            this.a1();
+            this.a1().a2().a3()
+                .a4().a5().a6()
+                .a7().a89().a10();
         };
         return classes;
     }());
     main.classes = classes;
+    //a10
+    var Animal = (function () {
+        function Animal(name) {
+            this.name = name;
+        }
+        return Animal;
+    }());
 })(main || (main = {}));
 /* program */
 (new main.classes()).start();
--- app/classes/scripts.ts
+++ app/classes/scripts.ts
@@ -25,11 +25,167 @@ module main {
             return this;
         }
         
+        a2() {
+            document.getElementById("a2")
+                .addEventListener("click", function() {
+                    (this as HTMLElement).innerText = 
+                        `Error: 'name' is private`;
+                });
+            return this;
+        }
+
+        a3() {
+            document.getElementById("a3")
+                .addEventListener("click", function() {
+                    (this as HTMLElement).innerText =
+                        `yes`;
+                });
+            return this;
+        }
+
+        a4() {
+            document.getElementById("a4")
+                .addEventListener("click", function() {
+                    (this as HTMLElement).innerText =
+                        `no`;
+                });
+            return this;
+        }
+
+        a5() {
+            class Person {
+                protected name: string;
+                constructor(name: string) { this.name = name; }
+            }
+
+            class Employee extends Person {
+                private department: string;
+
+                constructor(name: string, department: string) {
+                    super(name);
+                    this.department = department;
+                }
+
+                public getElevatorPitch() {
+                    return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+                }
+            }
+
+            document.getElementById("a5")
+                .addEventListener("click", function() {
+                    let howard = new Employee("Howard", "Sales");
+                    (this as HTMLElement).innerText =
+                        howard.getElevatorPitch();
+                });
+            return this;
+        }
+
+        a6() {
+            document.getElementById("a6")
+                .addEventListener("click", function() {
+                    (this as HTMLElement).innerText =
+                        `error`;
+                });
+            return this;
+        }
+
+        a7() {
+            let passcode = "secret passcode";
+
+            class Employee {
+                private _fullName: string;
+
+                get fullName(): string {
+                    return this._fullName;
+                }
+
+                set fullName(newName: string) {
+                    if (passcode && passcode == "secret passcode") {
+                        this._fullName = newName;
+                    }
+                    else {
+                        console.log("Error: Unauthorized update of employee!");
+                    }
+                }
+            }
+
+            
+            document.getElementById("a7")
+                .addEventListener("click", function() {
+                    let employee = new Employee();
+                    employee.fullName = "Bob Smith";
+                    if (employee.fullName) {
+                        console.log(employee.fullName);
+                    }
+
+                    (this as HTMLElement).innerText =
+                        `${employee.fullName}`;
+                });
+            return this;
+        }
+
+        a89() {
+            class Grid {
+                static origin = {x: 0, y: 0};
+                calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+                    let xDist = (point.x - Grid.origin.x);
+                    let yDist = (point.y - Grid.origin.y);
+                    return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+                }
+                constructor (public scale: number) { }
+            }
+
+            let grid1 = new Grid(1.0);  // 1x scale
+            let grid2 = new Grid(5.0);  // 5x scale
+
+            document.getElementById("a8")
+                .addEventListener("click", function() {
+                    (this as HTMLElement).innerText =
+                        `${grid1.calculateDistanceFromOrigin({x: 10, y: 10})}`;
+                });
+
+            document.getElementById("a9")
+                .addEventListener("click", function() {
+                    (this as HTMLElement).innerText =
+                        `${grid2.calculateDistanceFromOrigin({x: 10, y: 10})}`;
+                });
+            return this;
+        }
+
+        a10() {
+            class Dog extends Animal {
+                constructor() {
+                    super("puppy");
+                }
+
+                talk() {
+                    return `${this.name} is talking`;
+                }
+            }
+
+            document.getElementById("a10")
+                .addEventListener("click", function() {
+                    let dog = new Dog();
+                    (this as HTMLElement).innerText =
+                        `${dog.talk()}`;
+                });
+            return this;
+        }
+
         start() {
-            this.a1();
+            this.a1().a2().a3()
+                .a4().a5().a6()
+                .a7().a89().a10();
         }
     }
 
+    //a10
+    abstract class Animal {
+        constructor(public name: string){
+
+        }
+        protected abstract talk(): string;
+    }
 }
 
 /* program */
